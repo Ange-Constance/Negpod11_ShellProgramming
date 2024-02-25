7ee38785b44a667e02c84845117a39868a92d9e9
JEASA <jean-luc.sauve@ist.com>
2024-02-25 21:28:59 +0200

    added comments to explain commands

--- move-to-directory.sh
+++ move-to-directory.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# Indicate the chosen folder name
 directory_name="negpod_11-q1"
 
+# Establish the directory if it doesn't exist
 mkdir -p "$directory_name"
 
+# Transfer the files to the specified directory
+# Presuming the files are in the current directory and labeled file1, file2, file3, file4
+# Adjust the file names based on the files you intend to move
 mv main.sh students-list_1023.txt select-emails.sh student-emails.txt "$directory_name/"
