ac17642b31ca4a5fa589788e371e82e15bf7b337
ulrich <ulrichskyler7@gmail.com>
2024-02-25 21:58:59 +0200

    Configuration

---
+++ backup-Negpod_11.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Configuration parameters
+remote_server="64293e56bc62.3a2627c1.alu-cod.online"
+remote_user="64293e56bc62"
+remote_secret="328d3b338a4ced526c9a"
+remote_location="/summative/1023-2024j"
+local_location="negpod_11-q1"
